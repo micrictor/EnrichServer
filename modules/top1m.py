49be64b86dd703f203c747e008fb0cea4b3a4ca0
micrictor <mic.ric.tor@gmail.com>
2019-09-06 18:42:36 -0700

    Create Umbrella Top1M module

---
+++ modules/top1m.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import BaseEnricher
+import json,csv
+
+class Enricher(BaseEnricher.BaseEnricher):
+	METADATA = {
+		'AUTHOR':'Michael R. Torres',
+		'NAME':'Top1M',
+		'DESCRIPTION':'Returns rank of the domain in Cisco OpenDNS Top1m domains or 0',
+		'VERSION':'1.0',
+		'PARAMETERS':'data: The domain to return the ranking of.'
+	}
+	shouldCache = True
+	
+	def do_enrich(self, args):
+		if( args.get('data') == None ):
+			return "0"
+			
+		target = args['data'].lower()
+		
+		value = 0
+		with open('resources/top1m/top-1m.csv', 'r') as top1m_fl:
+			csv_obj = csv.reader(top1m_fl)
+			for row in csv_obj:
+				if( row[1] == target ):
+					value = row[0]
+					break
+
+		return str(value)
